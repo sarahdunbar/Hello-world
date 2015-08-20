09dc960ff41e50fcc3e4bef0d6664875316ab5a7
Eric Dennison <ericd@netdenizen.com>
2015-08-20 08:51:47 -0400

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# helloworld.py
+# Author: <your name here>
+# Credit: <list sources used, if any>
+#
+# Assignment:
+#
+# Write and submit a Python program that prints the following:
+#
+# Hello, world!
+#
+
