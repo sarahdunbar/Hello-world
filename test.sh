41323fe129c14c9c33d3926e29124623a5c2d1e1
Eric Dennison <ericd@netdenizen.com>
2015-08-21 14:47:57 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 set -e # exit with nonzero exit code if anything fails
 
 # create the tests folder
-git clone https://${GH_TOKEN}@github.com/HHS-IntroProgramming-Fall2015/tests.git
+git clone "https://${GH_TOKEN}@github.com/HHS-IntroProgramming-Fall2015/tests.git"
 
 python tests/test.py ${TESTMODULE}
