7dc35c952b82ad91dd0505d6719d1fdaa881387b
Eric Dennison <ericd@netdenizen.com>
2015-08-21 12:59:50 -0400

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,12 @@
-# helloworld.py
-# Author: <your name here>
-# Credit: <list sources used, if any>
-#
-# Assignment:
-#
-# Write and submit a Python program that prints the following:
-#
-# Hello, world!
-#
+"""
+helloworld.py
+Author: <your name here>
+Credit: <list sources used, if any>
+
+Assignment:
+
+Write and submit a Python program that prints the following:
+
+Hello, world!
+"""
 
